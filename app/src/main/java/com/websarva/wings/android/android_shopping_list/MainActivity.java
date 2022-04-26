6eee0a2cf90bbc989fe082416209c790c3c116b5
Sodai-Komichi <83051052+Aso2101122@users.noreply.github.com>
2022-04-26 21:36:57 +0900

    アイテム追加機能追加

--- app/src/main/java/com/websarva/wings/android/android_shopping_list/MainActivity.java
+++ app/src/main/java/com/websarva/wings/android/android_shopping_list/MainActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -36,6 +42,12 @@ public class MainActivity extends AppCompatActivity {
         SimpleAdapter adapter = new SimpleAdapter(MainActivity.this, _unCompleteList, R.layout.row, FROM, TO);
         //アダプタの登録
         _lvItem.setAdapter(adapter);
+
+
+        //追加ボタンオブジェクトを取得
+        Button addBtn = findViewById(R.id.item_add_btn);
+        //追加ボタンににリスナを設定
+        addBtn.setOnClickListener(new btnClickListener());
     }
 
     @Override
@@ -67,8 +79,38 @@ public class MainActivity extends AppCompatActivity {
             item.put("complete_flag", cursor.getString(idxComplete_flag));
             itemList.add(item);
         }
+        cursor.close();
         return itemList;
     }
 
+    //追加ボタンを押したときのリスナクラス
+    private class btnClickListener implements View.OnClickListener{
+        @Override
+        public void onClick(View view){
+            if(view.getId() == R.id.item_add_btn){
+                //追加ボタンが押された時の処理
+                //ボタンが押されているか
+                Log.d("tag","Onclick");
+                //アイテム名を取得する
+                EditText edItemName = findViewById(R.id.item_name_et);
+                String itemName = edItemName.getText().toString();
 
+                //データベースヘルパーオブジェクトからデータベース接続オブジェクトを取得
+                SQLiteDatabase db = _helper.getWritableDatabase();
+                //インサート用SQL文字列の用意
+                String sqlInsert = "INSERT INTO shopping_list (item_name, quantity, complete_flag) VALUES (?,1,0)";
+                //SQL文字列をもとにプリペアドステートメントを取得
+                SQLiteStatement stmt = db.compileStatement(sqlInsert);
+                //変数のバインド
+                stmt.bindString(1, itemName);
+                //インサートSQLの実行
+                stmt.executeInsert();
+
+                edItemName.setText("");
+                Toast.makeText(MainActivity.this, "アイテムを追加しました。", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    
 }
